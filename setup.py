315eac00b8c783a0fe3bd9fb7cc0a33d81d244ae
cwatterworth <cwatterworth@oakland.edu>
2022-06-12 22:41:19 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='whatif',
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    version='0.1.0',
+    description='A short description of the project.',
+    author='chelsea',
+    license='',
+)
